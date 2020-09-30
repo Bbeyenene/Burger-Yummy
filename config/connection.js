a2d143e3d277e7abd7f39170384a2c8b11ad99f2
berhane beyene <monoxica2004@yahoo.com>
2020-09-30 18:50:33 -0500

    modification

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 var mysql = require("mysql");
-  //creating connection to mysql workbench 
+  //create connection
   connection = mysql.createConnection({
       host: "localhost",
       port: 3306,
       user: "root",
-      password: "Nathan@2015",
+      password: "",
       database: "burgers_db"
   });
-//connect the connection
+//connect the connection to mysql
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -15,5 +15,5 @@ connection.connect(function(err) {
   }
   console.log("connected as id: " + connection.threadId);
 });
-//export the connection to be used by the different routes for the interaction b/n mysql and body like terminal or browser
+//export the connection to be used by other files
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -7,15 +7,18 @@ function printQuestionMarks(num) {
   for (var i = 0; i < num; i++) {
     arr.push("?");
   }
+
   return arr.toString();
 }
 
 function objToSql(ob) {
+  // column1=value, column2=value2,...
   var arr = [];
 
   for (var key in ob) {
     arr.push(key + "=" + ob[key]);
   }
+
   return arr.toString();
 }
 
@@ -30,6 +33,8 @@ var orm = {
     });
   },
 
+  // vals is an array of values that we want to save to cols
+  // cols are the columns we want to insert the values into
   create: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
@@ -40,6 +45,8 @@ var orm = {
     queryString += printQuestionMarks(vals.length);
     queryString += ") ";
 
+    //console.log(queryString);
+
     connection.query(queryString, vals, function(err, result) {
       if (err) {
         throw err;
@@ -48,6 +55,8 @@ var orm = {
     });
   },
   
+  // objColVals would be the columns and values that you want to update
+  // an example of objColVals would be {name: panther, sleepy: true}
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -56,6 +65,7 @@ var orm = {
     queryString += " WHERE ";
     queryString += condition;
 
+    //console.log(queryString);
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
