7adea9cf1a71fabe2333a6843dd68dded9741a23
berhane beyene <monoxica2004@yahoo.com>
2020-09-30 16:22:54 -0500

    comment added to the final commit

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-
+  //creating connection to mysql workbench 
   connection = mysql.createConnection({
       host: "localhost",
       port: 3306,
@@ -7,7 +7,7 @@ var mysql = require("mysql");
       password: "Nathan@2015",
       database: "burgers_db"
   });
-
+//connect the connection
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -15,5 +15,5 @@ connection.connect(function(err) {
   }
   console.log("connected as id: " + connection.threadId);
 });
-
+//export the connection to be used by the different routes for the interaction b/n mysql and body like terminal or browser
 module.exports = connection;
