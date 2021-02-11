0967b25138ed1713bd5cf001b07f136cc51744f5
berhane beyene <monoxica2004@yahoo.com>
2021-02-11 00:26:40 -0600

    controllers/bergersController.js modidified

--- controllers/bergersController.js
+++ controllers/bergersController.js
@@ -2,22 +2,22 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../model/berger.js");
 
-// use express for the / route response  run the /burgers route 
-router.get("/", function(req, res) {
+// use express for the / route response  run the /burgers route
+router.get("/", (req, res) => {
   res.redirect("/burgers");
 });
 
 //use /burgers route to render all the data from mysql to index using hundlebars
-router.get("/burgers", function(req, res) {
-  burger.all(function(burgerData) {
+router.get("/burgers", (req, res) => {
+  burger.all((burgerData) => {
     res.render("index", { burger_data: burgerData });
   });
 });
 
 // use the /burgers/create route to get the data submited from inputs in the browser and post it to the mysql and also redirect to the browser too
-router.post("/burgers/create", function(req, res) {
-  burger.create(req.body.burger_name, function(result) {  
-      res.redirect("/");
+router.post("/burgers/create", (req, res) => {
+  burger.create(req.body.burger_name, (result) => {
+    res.redirect("/");
   });
 });
 
@@ -25,8 +25,8 @@ router.post("/burgers/create", function(req, res) {
 // wrapper for orm.js that using MySQL update callback will return a log to console,
 // render back to index with handle
 // Send back response and let page reload from .then in Ajax
-router.put("/burgers/:id", function(req, res) {
-  burger.update(req.params.id, function(result) { 
+router.put("/burgers/:id", (req, res) => {
+  burger.update(req.params.id, (result) => {
     //console.log(result);
     res.sendStatus(200);
   });
