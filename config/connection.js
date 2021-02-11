0393c0f6ffe071bc76f704fb156a2351fb2d6dd2
berhane beyene <monoxica2004@yahoo.com>
2021-02-11 00:24:49 -0600

    config/connection.js modified

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,24 @@
 var mysql = require("mysql");
 var connection;
 
-  //create connection
-  if (process.env.JAWSB_URL) {
-    connection = mysql.createConnection(process.env.JAWSB_URL);
-  } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: null,
-        database: "burgers_db"
-    });
-  }
+//create connection
+if (process.env.JAWSB_URL) {
+  connection = mysql.createConnection(process.env.JAWSB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Nathan@2015",
+    database: "burgers_db",
+  });
+}
 //connect the connection to mysql
-connection.connect();
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   } console.log("connected as id: " + connection.threadId);
-// });
+
+connection.connect((err) => {
+  if (err) console.error("error connecting: " + err.stack);
+  return;
+  console.log("connected as id: " + connection.threadId);
+});
 //export the connection to be used by other files
 module.exports = connection;
