3c085f671b65a6362d62b11d2ae6d805bb0a5ad4
berhane beyene <monoxica2004@yahoo.com>
2021-02-11 00:27:16 -0600

    model/berger.js modidified

--- model/berger.js
+++ model/berger.js
@@ -3,21 +3,15 @@ var orm = require("../config/orm.js");
 
 var burger = {
   //from burgers table get a response of all the data
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(name, cb) {
-      // use burgers table burger_name column and add it to the not devoured list
-    orm.create("burgers", ["burger_name", "devoured"], [name, false], cb);
-  },
-  
-  update: function(id, cb) {
-    //to update devoured pass the id of that devoured and pass that value to devored list
-    var condition = "id=" + id;
-    orm.update("burgers", { devoured: true }, condition, cb);
-  }
+  all: (cb) => orm.all("burgers", (res) => cb(res)),
+
+  // use burgers table burger_name column and add it to the not devoured list
+  create: (name, cb) =>
+    orm.create("burgers", ["burger_name", "devoured"], [name, false], cb),
+
+  //to update devoured pass the id of that devoured and pass that value to devored list
+  update: (id, cb) =>
+    orm.update("burgers", { devoured: true }, `id= ${id}`, cb),
 };
-//export burgers constructor
+
 module.exports = burger;
